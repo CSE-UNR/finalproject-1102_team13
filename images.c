9ddbd84114fe2d2bff440dd325fd56171f116143
1102-Martinez-Uvensis <uvensismartinez@gmail.com>
2024-05-03 21:13:16 +0000

    1st try

--- images.c
+++ images.c
@@ -4,11 +4,25 @@
 
 
 #include<stdio.h>
-#include<stdbool>
+
+
 
 
 int main(){
+int userschoice;
 
+do{
+printf("\nWelcome to Erinstagram\n");
+printf("[1]-Load Your Image\n");
+printf("[2]-Display Your Image\n");
+printf("[3]-Edit The Image\n");
+printf("[4]-Exit\n");
+scanf("%d", &userschoice);
+
+if(userschoice ='4'){
+printf("\nThank you for choosing Erinstagram\n");
+printf("\nGoodbye!\n");}   
+}while(userschoice =! '4');   
 
 return 0;
 }
